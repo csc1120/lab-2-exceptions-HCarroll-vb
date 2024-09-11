d9e9d80f5c5cfc815da766cd60fa222a53478a92
HCarroll-vb <carrollh@msoe.edu>
2024-09-11 15:28:55 -0500

    Completed DieNotRolledException class

---
+++ src/carrollh/DieNotRolledException.java
@@ -0,0 +1,22 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * DieNotRolledException class
+ * Name: Hunter Carroll
+ * Last Updated: 9/11/2024
+ */
+package carrollh;
+
+/**
+ * DieNotRolledException class to test if currentValue is in range of Die
+ */
+public class DieNotRolledException extends RuntimeException {
+
+    /**
+     * Message that not correct type is used
+     * @return exception message if thrown
+     */
+    public String getMessage(){
+        return "currentValue not in expected range of die";
+    }
+}
